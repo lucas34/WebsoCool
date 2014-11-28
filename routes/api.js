cce03e404acf6882315dc4d25b5f3539262a56a3
Ferrand <Crikklayer@gmail.com>
2014-11-28 20:29:22 +0100

    Du bon js ! (lib principal)

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var chat = require('../lib/websocool');
 
 /* GET users listing. */
 router.get('/polling', function(req, res) {
-  res.send('respond with a resource');
+  chat.createRoom('hello');
+  res.send(chat.getListOfRooms());
 });
 
 router.get('/long-polling', function(req, res) {
