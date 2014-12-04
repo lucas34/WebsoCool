2d7b16f89e546af41313a6cf21e3970e9005ed3f
lucas34 <lucas34990@gmail.com>
2014-12-04 12:03:19 +0100

    BUG : Erreur 500 postMessage

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
+/*
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -58,7 +59,7 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
+*/
 // --------- Socket ------------
 (function() {
     io.on('connection', function (socket) {
